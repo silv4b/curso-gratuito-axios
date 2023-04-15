bd5c84b1d3559eed6a4272cff7d98bd0090167e1
Bruno Silva <brunosilva@viggosistemas.com.br>
2023-04-14 21:59:26 -0300

    get request with axios

--- src/js/main.js
+++ src/js/main.js
@@ -4,76 +4,90 @@ const headersEl = document.getElementById('headers');
 const configEl = document.getElementById('config');
 
 const get = () => {
-    console.log('get');
+	/*axios({
+			method: 'get',
+			url: 'https://jsonplaceholder.typicode.com/posts'
+	})*/
+
+	axios.get('https://jsonplaceholder.typicode.com/posts', {
+		// https://jsonplaceholder.typicode.com/posts?_limit=5 or
+		params: {
+			_limit: 5
+		}
+	})
+		.then((response) => {
+			renderOutput(response)
+		})
+	console.log('get');
 }
 
 const post = () => {
-    console.log('post');
+	console.log('post');
 }
 
 const put = () => {
-    console.log('put');
+	console.log('put');
 }
 
 const patch = () => {
-    console.log('patch');
+	console.log('patch');
 }
 
 const del = () => {
-    console.log('delete');
+	console.log('delete');
 }
 
 const multiple = () => {
-    console.log('multiple');
+	console.log('multiple');
 }
 
 const transform = () => {
-    console.log('transform');
+	console.log('transform');
 }
 
 const errorHandling = () => {
-    console.log('errorHandling');
+	console.log('errorHandling');
 }
 
 const cancel = () => {
-    console.log('cancel');
+	console.log('cancel');
 }
 
 const clear = () => {
-    statusEl.innerHTML = '';
-    statusEl.className = '';
-    dataEl.innerHTML = '';
-    headersEl.innerHTML = '';
-    configEl.innerHTML = '';
+	statusEl.innerHTML = '';
+	statusEl.className = '';
+	dataEl.innerHTML = '';
+	headersEl.innerHTML = '';
+	configEl.innerHTML = '';
 }
 
 const renderOutput = (response) => {
-    // Status
-    const status = response.status;
-    statusEl.removeAttribute('class');
-    let statusElClass = 'inline-flex items-center px-2.5 py-0.5 rounded-md text-sm font-medium';
-    if (status >= 500) {
-        statusElClass += ' bg-red-100 text-red-800';
-    } else if (status >= 400) {
-        statusElClass += ' bg-yellow-100 text-yellow-800';
-    } else if (status >= 200) {
-        statusElClass += ' bg-green-100 text-green-800';
-    }
+	// Status
+	const status = response.status;
+	statusEl.removeAttribute('class');
+	let statusElClass = 'inline-flex items-center px-2.5 py-0.5 rounded-md text-sm font-medium';
+	if (status >= 500) {
+		statusElClass += ' bg-red-100 text-red-800';
+	} else if (status >= 400) {
+		statusElClass += ' bg-yellow-100 text-yellow-800';
+	} else if (status >= 200) {
+		statusElClass += ' bg-green-100 text-green-800';
+	}
 
-    statusEl.innerHTML = status;
-    statusEl.className = statusElClass;
+	statusEl.innerHTML = status;
+	statusEl.className = statusElClass;
 
-    // Data
-    dataEl.innerHTML = JSON.stringify(response.data, null, 2);
-    Prism.highlightElement(dataEl);
+	// Data
+	dataEl.innerHTML = JSON.stringify(response.data, null, 2);
+	Prism.highlightElement(dataEl);
 
-    // Headers
-    headersEl.innerHTML = JSON.stringify(response.headers, null, 2);
-    Prism.highlightElement(headersEl);
+	// Headers
+	headersEl.innerHTML = JSON.stringify(response.headers, null, 2);
+	Prism.highlightElement(headersEl);
 
-    // Config
-    configEl.innerHTML = JSON.stringify(response.config, null, 2);
-    Prism.highlightElement(configEl);
+	// Config
+	configEl.innerHTML = JSON.stringify(response.config, null, 2);
+	Prism.highlightElement(configEl);
 }
 
 document.getElementById('get').addEventListener('click', get);
